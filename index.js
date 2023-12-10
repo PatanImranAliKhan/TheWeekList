4f32e414eb861720dd8f1f3b3531d6b15df6886d
Patan Khan <patan_khan@bonify.de>
2023-12-10 22:16:31 +0530

    mongo connect

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.listen(3000, () => {
-    mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true,
+app.listen(3000, async () => {
+    await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true,
         useUnifiedTopology: true})
+        .then(() => {
+            console.log("mongodb connected")
+        })
     console.log("Connect to PORT 3000");
 })
